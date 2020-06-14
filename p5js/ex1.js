071cd5723161a78767241bd674ed9585f71660d9
rita2014189 <57712522+rita2014189@users.noreply.github.com>
2020-06-14 18:22:36 +0100

    Update ex1.js

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,23 +1,9 @@
 //Variáveis tipo let nomeDaVar;
-let posX, posY;
-let velX, velY;
-let padWidth, padHeight;
-let diamBall;
-let points;
-let vel;
 
 function setup() { //código que só corre uma vez
-  createCanvas(600, 600);
-  posX = random(width);
-  posY = 30;
-  vel = 5;
-  velX = vel;
-  velY = vel;
-  padWidth = 80;
-  padHeight = 8;
-  diamBall = 15;
-  points=0;
-}
+  
+createCanvas(640, 480);
+  
   ///Não Apagar para Baixo até <<<
   var canvasDiv = document.getElementById('ex1');
   var width = canvasDiv.offsetWidth;
@@ -30,49 +16,75 @@ function setup() { //código que só corre uma vez
 }
 
 function draw() { //código que corre em loop
- background(0,100,100);
-  fill(255, 204, 0);
-  noStroke();
-  text(points,10,20);
-  
-  posX = posX + velX;
-  posY = posY + velY;
-  
-  fill(51);
-  noStroke();
-  ellipse(posX,posY,diamBall);
-  
-  noFill();
-  stroke(255, 204, 0);
-  strokeWeight(4);
-  rectMode(CENTER);
-  
-  rect(mouseX,height-10,padWidth,padHeight);
+ //desenha
+   background(200, 220, 260);
   
 
-
-  //calcula
-  if (posX-diamBall/2 <= 0 || posX+diamBall/2 >= width) {
-    velX = velX*-1;
-  }
-  if (posY-diamBall/2 <= 0) {
-    velY = velY*-1;
-  }
-
-  if (posY+diamBall/2 >= height){
-    print("Loser");
-    velX = 0;
-    velY = 0;
-    points = "Loser!";
-  }
-
-  if (velX != 0 && posX >= mouseX-padWidth/2 && posX <= mouseX+padWidth/2 && posY+diamBall/2 >= height-10-padHeight/2){
-    velY = -random(vel-3,vel);
-    velX =random(-vel,vel);
-    points++;
-  } 
-
-
-}
-
   
+//head  
+fill(254, 129, 161);
+noStroke();
+rect(200, 50, 250,200, 260); 
+  
+//hair
+noStroke(); 
+fill(254, 110, 200 );
+ellipse(245 ,46,40,40);
+fill(254 ,130, 200);
+ellipse(276 ,38,40,40); 
+  fill(254, 110, 200 );
+ellipse(310 ,35,40,40); 
+  fill(254 ,130, 200);
+ellipse(340 ,35,40,40);
+  fill(254, 110, 200 );
+ellipse(370 ,37,40,40); 
+  fill(254 ,130, 200);
+ellipse(400 ,44,40,40);
+  
+//face
+fill(181,200,255 ); 
+ellipse(410, 170, 50, 50); 
+ellipse(240, 170, 50, 50); 
+  
+//ears  
+fill(254, 129, 161);
+ellipse(462, 150, 30, 50); 
+ellipse(188, 150, 30, 50); 
+  
+ //eyes
+fill(181,20,285);
+ellipse(290, 100, 50, 20); 
+ellipse(350, 100, 50, 20); 
+  
+fill(181,270,255);  
+ellipse(290, 100, 15, 35); 
+ellipse(350, 100, 15, 35); 
+
+//mouth
+fill(192, 4, 52 ); 
+ellipse(325 ,200,30, 50, 335,170 ); 
+  
+  
+  
+  
+//body  
+fill(181,20,285); 
+ellipse(325, 345, 170, 200)
+  
+noStroke();
+fill(181,20,285 );
+ellipse(253,270,60,20); 
+
+
+noStroke();
+fill(181,20,285);
+ellipse(400 ,270,60,20); 
+
+noStroke();
+fill(181,20,285 );
+ellipse(280,450,45,35);
+ellipse(370,450,45,35);
+  
+
+
+} 
